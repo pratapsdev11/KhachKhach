81719ea19d514226016b88ddcfa3fdd07942e794
DivyaPratapSingh <109823056+pratapsdev11@users.noreply.github.com>
2024-08-26 00:07:09 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,3 +4,4 @@ from .xyn_extractor import XYNExtractor
 from .bounding_box_processor import BoundingBoxProcessor
 from .extended_array_processor import ExtendedArrayProcessor
 from .file_appender import FileAppender
+from .detectannotate import detection_annotation

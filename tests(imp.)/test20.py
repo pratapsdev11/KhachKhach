7efebc62d240d77a8a7722fca08a05d8784bcf48
DivyaPratapSingh <109823056+pratapsdev11@users.noreply.github.com>
2024-08-26 08:08:14 +0530

    Delete tests(imp.) directory

--- tests(imp.)/test20.py
+++
@@ -1,7 +0,0 @@
-from khach_khach import XYNExtractor
-
-annotations_folder = 'annotations'
-xyncontext_folder = 'xyncontext'
-
-extractor = XYNExtractor(annotations_folder, xyncontext_folder)
-extractor.extract_xyn_array()
